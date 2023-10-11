f0a70b6c231bb98b3a4117ec5449815dcb7b85c6
Dominik <Dominik@example.com>
2023-10-11 15:19:54 +0200

    added local path variable

--- Copy_file.py
+++ Copy_file.py
@@ -1,19 +1,21 @@
 import shutil
 import time
 
+directory_path = 'C:\\SSIS\\SSIS_DataFlow'
+
 date = time.strftime("%d-%m-%Y")
 renton_date = "Input_Rentone_" + date
 Seattle_date = "Input_Seattle_" + date
 Kenmore_date = "Input_Kenmore_" + date
 
-Renton_original = "C:\SSIS\Integration_Services_Project1\Input_Renton.txt"
-Renton_backup = f"C:\SSIS\Integration_Services_Project1\\backup_files\{renton_date}.txt"
+Renton_original = f"{directory_path}\\Input_Renton.txt"
+Renton_backup = f"{directory_path}\\backup_files\\{renton_date}.txt"
 shutil.copy(Renton_original, Renton_backup)
 
-Seattle_original = "C:\SSIS\Integration_Services_Project1\Input_Seattle.txt"
-Seattle_backup = f"C:\SSIS\Integration_Services_Project1\\backup_files\{Seattle_date}.txt"
+Seattle_original = f"{directory_path}\\Input_Seattle.txt"
+Seattle_backup = f"{directory_path}\\backup_files\\{Seattle_date}.txt"
 shutil.copy(Seattle_original, Seattle_backup)
 
-Kenmore_original = "C:\SSIS\Integration_Services_Project1\Input_Kenmore.txt"
-Kenmore_backup = f'C:\SSIS\Integration_Services_Project1\\backup_files\{Kenmore_date}.txt'
+Kenmore_original = f"{directory_path}\\Input_Kenmore.txt"
+Kenmore_backup = f'{directory_path}\\backup_files\\{Kenmore_date}.txt'
 shutil.copy(Kenmore_original, Kenmore_backup)
