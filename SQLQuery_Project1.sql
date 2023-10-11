a8ccb101fa4a8180b0793096792bb90ef6d89ad1
Dominik <Dominik@example.com>
2023-10-11 15:22:09 +0200

    increase number of characters in columns

--- SQLQuery_Project1.sql
+++ SQLQuery_Project1.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE My_PersonAdress(
+CREATE TABLE My_PersonAdress2(
 	[AddressID] [int],
 	[AddressLine1] [nvarchar](60) NOT NULL,
 	[AddressLine2] [nvarchar](60) NULL,
-	[City] [nvarchar](30) NOT NULL,
+	[City] [nvarchar](50) NOT NULL,
 	[StateProvinceID] [int] NOT NULL,
-	[PostalCode] [nvarchar](15) NOT NULL,
+	[PostalCode] [nvarchar](50) NOT NULL,
 	[ModifiedDate] [datetime] NOT NULL)
 
 CREATE TABLE SSIS_execute(
